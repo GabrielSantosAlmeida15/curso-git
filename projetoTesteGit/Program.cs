31db4dd4b33d40a78ad817ed81b72888dd66cf3e
Gabriel S Almeida <gabrielsantosalmeida15@gmail.com>
2020-11-25 14:35:10 -0300

    Projeto iniciado mostrando ola mundo

---
+++ projetoTesteGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projetoTesteGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
